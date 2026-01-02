d4bf427ffe3a698ca9bb69e4ff45ff9579c27c3c
Temir <karabaevtemirlan69@gmail.com>
2026-01-03 04:03:52 +0600

    feat: Add no-op JVM implementation for VibrationHelper
    Adds a no-op implementation of the `VibrationHelper` for the JVM (Desktop) target.
    
    This commit creates `VibrationHelper.jvm.kt` to satisfy the `actual` requirement of the existing `expect` class. The JVM implementation contains empty methods for `triggerVibration` and `initialize`, as haptic feedback is not applicable on this platform. The `getInstance` composable is marked with `TODO()` as it is not yet implemented.

---
+++ composeApp/src/jvmMain/kotlin/com/kiu/smoothmines/utils/VibrationHelper.jvm.kt
@@ -0,0 +1,18 @@
+package com.kiu.smoothmines.utils
+
+import androidx.compose.runtime.Composable
+
+actual class VibrationHelper {
+    actual fun triggerVibration() {
+    }
+
+    actual fun initialize(context: Any) {
+    }
+
+    actual companion object {
+        @Composable
+        actual fun getInstance(): VibrationHelper {
+            TODO("Not yet implemented")
+        }
+    }
+}
